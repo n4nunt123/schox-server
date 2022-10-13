14765c565cad14b3c4b577fa70f3d2893b55aee4
isan <ihellostereo@gmail.com>
2022-10-13 11:57:54 +0700

    Update: finishing socket IO

--- socket/socketConfig.js
+++ socket/socketConfig.js
@@ -18,6 +18,16 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("recieve:interval", data);
   });
 
+  socket.on('send:coordinate-customer', (data) => {
+    console.log(data, 'coordinat customer')
+    socket.broadcast.emit('recieve:coordinate-customer', data)
+  })
+
+  socket.on('send:status-driver', (data) => {
+    console.log(data, 'status driver')
+    socket.broadcast.emit('recieve:status-driver', data)
+  })
+
   socket.on("disconnect", () => {
     console.log(`connection with socket disconnected`);
   });
